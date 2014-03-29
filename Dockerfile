2e44be5970db39764fab7e81c3479e7bbc3974e6
Tyler Hall <tylerwhall@gmail.com>
2014-03-29 17:27:26 -0400

    Add gitweb support

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,13 @@ ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update
 RUN apt-get install -y openjdk-6-jre-headless git-core vim-tiny curl
+RUN apt-get install -y gitweb
 
 ENV GERRIT_HOME /home/gerrit
 
 RUN useradd -m gerrit
 USER gerrit
 RUN curl http://gerrit-releases.storage.googleapis.com/gerrit-2.8.3.war -o $GERRIT_HOME/gerrit.war
-RUN java -jar $GERRIT_HOME/gerrit.war init --batch -d $GERRIT_HOME
 ADD start-gerrit.sh $GERRIT_HOME/start-gerrit.sh
 VOLUME ["/home/gerrit"]
 
--- start-gerrit.sh
+++ start-gerrit.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-git config --file $GERRIT_HOME/etc/gerrit.config database.type 'Postgresql'
-git config --file $GERRIT_HOME/etc/gerrit.config database.hostname $DB_PORT_5432_TCP_ADDR
-git config --file $GERRIT_HOME/etc/gerrit.config database.port $DB_PORT_5432_TCP_PORT
-git config --file $GERRIT_HOME/etc/gerrit.config database.database $POSTGRESQL_DB
-git config --file $GERRIT_HOME/etc/gerrit.config database.username $POSTGRESQL_USER
-git config --file $GERRIT_HOME/etc/gerrit.config database.password $POSTGRESQL_PASS
-java -jar $GERRIT_HOME/bin/gerrit.war init -d $GERRIT_HOME
+set -x
+set -e
+
+if [ ! -e $GERRIT_HOME/initialized ]; then
+    mkdir -p $GERRIT_HOME/etc
+    git config --file $GERRIT_HOME/etc/gerrit.config database.type 'Postgresql'
+    git config --file $GERRIT_HOME/etc/gerrit.config database.hostname $DB_PORT_5432_TCP_ADDR
+    git config --file $GERRIT_HOME/etc/gerrit.config database.port $DB_PORT_5432_TCP_PORT
+    git config --file $GERRIT_HOME/etc/gerrit.config database.database $POSTGRESQL_DB
+    git config --file $GERRIT_HOME/etc/gerrit.config database.username $POSTGRESQL_USER
+    git config --file $GERRIT_HOME/etc/gerrit.config database.password $POSTGRESQL_PASS
+    git config --file $GERRIT_HOME/etc/gerrit.config gitweb.cgi /usr/lib/cgi-bin/gitweb.cgi
+    git config --file $GERRIT_HOME/etc/gerrit.config --unset gitweb.url || true
+    java -jar $GERRIT_HOME/gerrit.war init --batch -d $GERRIT_HOME
+    touch $GERRIT_HOME/initialized
+fi
 exec $GERRIT_HOME/bin/gerrit.sh run
